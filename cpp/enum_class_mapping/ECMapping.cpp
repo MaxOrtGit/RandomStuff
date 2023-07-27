aac03eac1bd1d03cfc79035ed4a032c51f1b6d51
MaxOrtGit <70595776+MaxOrtGit@users.noreply.github.com>
2023-07-27 09:35:44 -0500

    made a bit better with a macro and decltype

--- cpp/enum_class_mapping/ECMapping.cpp
+++ cpp/enum_class_mapping/ECMapping.cpp
@@ -12,28 +12,41 @@ enum class Classes { CLASSES_LIST };
 template<typename Function>
 using FuncVec = std::vector<std::function<Function>>;
 
+#define MAKE_VECTOR_FUNCTION(func) \
+  template<typename... Args> \
+  auto Make##func##Vector() { \
+    return FuncVec<decltype(func<BaseClass>)>{func<Args>...}; \
+  }
+
+#define CreateVectorOfFunctions(variableName, func) \
+  MAKE_VECTOR_FUNCTION(func) \
+  auto variableName = Make##func##Vector<CLASSES_LIST>();
+
+/* example of CreateVectorOfFunctions(constructors, ConstructorInstance)
+
+  // ------------------ Function to return vector ------------------
+  template<typename... Args>
+  auto MakeConstructorInstanceVector() {
+    return FuncVec<decltype(ConstructorInstance<BaseClass>)>{ConstructorInstance<Args>...};
+  }
+
+  // ------------------ Creating the vector ------------------
+  auto constructors = MakeConstructorInstanceVector<CLASSES_LIST>();
+*/
+
 // ------------------ Function to create an instance ------------------
 template<typename T>
-std::unique_ptr<BaseClass> CreateInstance() {
+std::unique_ptr<BaseClass> ConstructorInstance() {
   return std::make_unique<T>();
 }
-
-template<typename... Args>
-auto MakeConstructorVector() {
-  return FuncVec<std::unique_ptr<BaseClass>()>{CreateInstance<Args>...};
-}
+CreateVectorOfFunctions(constructors, ConstructorInstance)
 
 // ------------------ Function call color on an instance ------------------
 template<typename T>
 void ColorInstance(int r, int g, int b) {
-    T::PrintColor(r, g, b);
+  T::PrintColor(r, g, b);
 }
-
-template<typename... Args>
-auto MakeColorVector() {
-  return FuncVec<void(int, int, int)>{ColorInstance<Args>...};
-}
-
+CreateVectorOfFunctions(colorers, ColorInstance)
 
 // ------------------ Using the functions ------------------
 
@@ -41,13 +54,11 @@ auto MakeColorVector() {
 // Function to create an instance based on the enum value
 std::unique_ptr<BaseClass> CreateObjectFromEnum(Classes enumValue) {
   // Creates a vector of functions that create an instance of the corresponding class
-  auto constructors = MakeConstructorVector<CLASSES_LIST>();
   int index = static_cast<int>(enumValue);
 
   // Calls the function and returns pointer to the instance
   return constructors[index]();
 }
-
 
 int main() {
   // Enum to object creation
@@ -57,7 +68,7 @@ int main() {
 
 
   // Constructs 10 random classes
-  auto constructors = MakeConstructorVector<CLASSES_LIST>();
+  auto constructors = MakeConstructorInstanceVector<CLASSES_LIST>();
   
   std::mt19937 rng(static_cast<unsigned>(std::time(0)));
   std::uniform_int_distribution<int> dist(0, constructors.size() - 1);
@@ -67,7 +78,6 @@ int main() {
   }
 
   // Colors all classes (calling function with arguments)
-  auto colorers = MakeColorVector<CLASSES_LIST>();
   for (auto& colorer : colorers) {
     colorer(1, 2, 3);
   }
