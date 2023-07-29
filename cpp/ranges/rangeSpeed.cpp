824b5061a3454b7413cdf863936633e4e2f561c9
MaxOrtGit <70595776+MaxOrtGit@users.noreply.github.com>
2023-07-29 17:42:21 -0500

    testing ranges speed for adding

---
+++ cpp/ranges/rangeSpeed.cpp
@@ -0,0 +1,78 @@
+#include <ranges>
+#include <vector>
+#include <iostream>
+#include <chrono>
+
+template<typename Func>
+auto measureExecutionTime(Func&& func, int iterations = 10000)
+{
+  auto start = std::chrono::high_resolution_clock::now();
+  for (int i = 0; i < iterations; ++i)
+    func();
+  auto end = std::chrono::high_resolution_clock::now();
+  return std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+}
+
+template<typename Func>
+void testFunc(Func&& func, int attempts = 10)
+{
+  for (int i = 0; i < attempts; ++i)
+    std::cout << measureExecutionTime(func) << ", ";
+  std::cout << std::endl;
+}
+
+auto v1 = std::views::iota(0, 1000);
+auto v2 = std::views::iota(1000, 2000);
+
+auto addVectors1()
+{
+  // add the two ranges
+  auto result = std::views::zip(v1, v2) | std::views::transform([](const auto& pair) {
+    return std::get<0>(pair) + std::get<1>(pair);
+  });
+}
+
+auto addVectors2()
+{
+  auto result = v1 | std::views::transform([&](int a) {
+    return a + *(std::next(v2.begin(), a));
+  });
+}
+
+auto addVectors3()
+{
+  auto result = v1 | std::views::transform([&](int a) {
+    return a + v2[a];
+  });
+}
+
+auto addVectors4()
+{
+  std::vector<int> result(v1.size());
+  for (std::size_t i = 0; i < v1.size(); ++i)
+    result[i] = v1[i] + v2[i];
+}
+
+auto addVectors5()
+{
+  std::vector<int> result(v1.size());
+  std::transform(v1.begin(), v1.end(), v2.begin(), result.begin(), std::plus<int>());
+}
+
+#define TestFunc(func) std::cout << "Execution time for " << #func << "(): " << std::endl; testFunc(func);
+#define TestFuncN(func, num) std::cout << "Execution time for " << #func << "(): " << std::endl; testFunc(func, num);
+
+int main()
+{
+  TestFunc(addVectors1);
+  
+  TestFunc(addVectors2);
+
+  TestFunc(addVectors3);
+  
+  TestFuncN(addVectors4, 1);
+  
+  TestFuncN(addVectors5, 1);
+
+  return 0;
+}
